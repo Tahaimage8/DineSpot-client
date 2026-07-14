1bf930ff8a7456ffc8f5219f679b6f207b5d6115
tahaimage8 <tahaimage8@gmail.com>
2026-07-14 06:52:46 +0600

    add: customer and restaurant owner registration

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,20 +2,34 @@
 
 import { ThemeProvider } from "next-themes";
 import type { ReactNode } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 type ProvidersProps = {
   children: ReactNode;
 };
 
-export default function Providers({ children }: ProvidersProps) {
+const Providers = ({ children }: ProvidersProps) => {
   return (
     <ThemeProvider
       attribute="class"
       defaultTheme="light"
       enableSystem={false}
       disableTransitionOnChange
+      storageKey="dinespot-theme"
     >
       {children}
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        theme="colored"
+      />
     </ThemeProvider>
   );
-}
+};
+
+export default Providers;
